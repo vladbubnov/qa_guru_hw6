39703c2ec775a665da1c43604f58b7b51916a327
vladislav.bubnov <v.bubnov@yclients.tech>
2024-11-28 22:27:07 +0200

    fix errors

--- test_homework.py
+++ test_homework.py
@@ -5,14 +5,41 @@ def activation_time_dark_theme(user_time):
     return time(hour=6) > user_time or user_time >= time(hour=22)
 
 
+def get_user_by_name(user_list, name):
+    for user in user_list:
+        if user["name"] == name:
+            return user
+
+
+def get_lowest_age(user_list, age):
+    new_users_list = []
+    for user in user_list:
+        if user["age"] < age:
+            new_users_list.append(user)
+    return new_users_list
+
+
+def enable_dark_theme(dark_theme_enabled_by_user, current_time):
+    if dark_theme_enabled_by_user is None:
+        return activation_time_dark_theme(current_time)
+    elif dark_theme_enabled_by_user:
+        return True
+    else:
+        return False
+
+
+def print_function_info(func, *args):
+    name = func.__name__
+    function_info = f"{name.title().replace("_", " ")} [{", ".join(args)}]"
+    print(function_info)
+    return function_info
+
+
 def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
     current_time = time(hour=23)
-
-    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-
     is_dark_theme = activation_time_dark_theme(current_time)
     assert is_dark_theme is True
 
@@ -28,22 +55,7 @@ def test_dark_theme_by_time_and_user_choice():
     current_time = time(hour=23)
     dark_theme_enabled_by_user = False
 
-    # TODO переключите темную тему в зависимости от времени суток,
-    #  но учтите что темная тема может быть включена вручную
-
-    # def enable_dark_theme():
-    #     if dark_theme_enabled_by_user is None:
-    #         return activation_time_dark_theme(current_time)
-    #     elif dark_theme_enabled_by_user:
-    #         return True
-    #     else:
-    #         return False
-
-    def enable_dark_theme():
-        return dark_theme_enabled_by_user or (
-                dark_theme_enabled_by_user is None and activation_time_dark_theme(current_time))
-
-    is_dark_theme = enable_dark_theme()
+    is_dark_theme = enable_dark_theme(dark_theme_enabled_by_user, current_time)
     assert is_dark_theme is False
 
 
@@ -59,23 +71,8 @@ def test_find_suitable_user():
         {"name": "Maria", "age": 18},
     ]
 
-    # TODO найдите пользователя с именем "Olga"
-    def get_user_by_name(user_list, name):
-        for user in user_list:
-            if user["name"] == name:
-                return user
-
     suitable_users = get_user_by_name(users, "Olga")
     assert suitable_users == {"name": "Olga", "age": 45}
-
-    # TODO найдите всех пользователей младше 20 лет
-
-    def get_lowest_age(user_list, age):
-        new_users_list = []
-        for user in user_list:
-            if user["age"] < age:
-                new_users_list.append(user)
-        return new_users_list
 
     suitable_users = get_lowest_age(users, 20)
     assert suitable_users == [
@@ -94,8 +91,6 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
-func.__name__
-
 
 def test_readable_function():
     open_browser(browser_name="Chrome")
@@ -104,15 +99,15 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_function_info(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_function_info(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_function_info(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
